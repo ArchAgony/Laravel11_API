3b6148fdeb36b36c4ffceb8c9622ad0b41513949
ArchAgony <davaariardiansyah01@gmail.com>
2024-12-02 21:00:12 +0700

    api (finish)

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -36,4 +36,41 @@ class APIController extends Controller
             ], 422);
         }
     }
+    public function updatestudent(Request $request, $id){
+        try {
+            $student = Student::find($id);
+            $student->nim = $request->input('nim');
+            $student->nama = $request->input('nama');
+            $student->gender = $request->input('gender');
+            $student->jurusan = $request->input('jurusan');
+            $student->keahlian = $request->input('keahlian');
+            $student->save();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'berhasil diubah'
+            ], 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'gagal diubah. Kesalahan: '.$e->getMessage(),
+            ], 401);
+        }
+    }
+
+    public function deletestudent($id){
+        $student = Student::find($id);
+        if ($student) {
+            $student->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'berhasil dihapus'
+            ], 200);
+        }else{
+            return response()->json([
+                'success' => false,
+                'message' => 'gagal dihapus. ID tidak ditemukan'
+            ], 401);
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,7 @@ Route::get('/user', function (Request $request) {
 Route::get('/students', [APIController::class, 'student']);
 
 Route::post('/students/add', [APIController::class, 'addstudent']);
+
+Route::put('/students/update/{id}', [APIController::class, 'updatestudent']);
+
+Route::delete('/students/delete/{id}', [APIController::class, 'deletestudent']);
